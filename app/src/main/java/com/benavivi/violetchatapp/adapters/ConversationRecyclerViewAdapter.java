6fc79496e58a9e9ee7bbe833931ec4c7b8f7466a
Sucralose <cafe.github.pe2xj@aleeas.com>
2023-12-12 21:34:40 +0200

    Added Conversation Recycler View

---
+++ app/src/main/java/com/benavivi/violetchatapp/adapters/ConversationRecyclerViewAdapter.java
@@ -0,0 +1,2 @@
+package com.benavivi.violetchatapp.adapters;public class ConversationRecyclerViewAdapter {
+}
--- app/src/main/java/com/benavivi/violetchatapp/adapters/GroupListAdapter/RecyclerViewOnClickInterface.java
+++
@@ -1,5 +0,0 @@
-package com.benavivi.violetchatapp.adapters.GroupListAdapter;
-
-public interface RecyclerViewOnClickInterface {
-	void onRecyclerViewItemClick (int position);
-}
